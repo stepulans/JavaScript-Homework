c881b3c1980d33a31a4e3664e10cb96c3d3f92c2
stepulans <123463857+stepulans@users.noreply.github.com>
2023-03-28 14:19:56 +0200

    Add files via upload

---
+++ Homework_2/Scripts/scripts.js
@@ -0,0 +1,33 @@
+//=================================Task 1==============================
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let index = 0; index < arr.length; index+= 2){
+//     console.log(arr[index])
+// }
+//=================================Task 2==============================
+// const arr = []
+// for (let index = 55; index >= 20; index--) {
+//     console.log(index)
+// }
+//=================================Task 3==============================
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// const numbers_squared = []
+// for (let i = 0; i < numbers.length; i++) {
+//     numbers_squared.push(numbers[i]**2)
+//     console.log(numbers_squared[i])
+// }
+//=================================Task 4===============================
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+//  const numbers_squared = []
+//  for (let i = 0; i < numbers.length; i++) {
+//      numbers_squared.push(numbers[i]**2)
+//  }
+//  const last_elem = numbers_squared[numbers_squared.length - 1]
+//  console.log(last_elem)
+ //================================Task 5===============================
+ const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+      console.log(`User's name is ${user.first_name} ${user.last_name} He is ${user.age} years old`)
